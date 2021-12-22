307e9b072aea16e789a04e574e0e492faf751d2a
jerkzilla <johnjamesmarshalljr@gmail.com>
2021-12-22 06:54:12 -0600

    logged game board in server, connected with socket

--- server/server.js
+++ server/server.js
@@ -6,10 +6,27 @@ const io = require('socket.io')(server, {
     }
 });
 io.on('connection', (socket)=> {
+    socket.on('custom-event', (number, string, obj) => {
+        console.log(number, string, obj)
+    })
+    socket.on("gameBoard", squares => {
+        console.log(squares)
+    })
     socket.on("play", index => {
         console.log("server received", index)
         socket.broadcast.emit("play", index)
     })
 })
 
+
+
 server.listen(3000)
+
+// const io = require('socket.io')(8080)
+
+// io.on('connection', socket =>  {
+//     console.log(socket.id)
+//     socket.on('custom-event', (number, string, obj) => {
+//         console.log(number, string, obj)
+//     })
+// })
